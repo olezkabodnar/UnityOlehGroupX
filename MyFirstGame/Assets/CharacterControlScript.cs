70752161ef0b36c0a30b907afe4c7f6525b4bf23
olezkabodnar <64169559+olezkabodnar@users.noreply.github.com>
2023-12-13 21:13:11 +0000

    Signed-off-by: olezkabodnar <64169559+olezkabodnar@users.noreply.github.com>

--- MyFirstGame/Assets/CharacterControlScript.cs
+++ MyFirstGame/Assets/CharacterControlScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class CharacterControlScript : MonoBehaviour
 {
-    public float speed = 0.3f;
-    public float mouseSensitivity = 3000000f;
+    public float speed = 0.1f;
+    public float mouseSensitivity = 300f;
+    public GameObject BulletLite_02; // Префаб кулі
+    public Transform firePoint; // Точка вистрілу
 
 
 
@@ -20,7 +22,12 @@ public class CharacterControlScript : MonoBehaviour
 
     
     void Update()
+
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
         float rotationX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;//move mouse
         float rotationY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         transform.Rotate(Vector3.up, rotationX);
@@ -33,5 +40,15 @@ public class CharacterControlScript : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         transform.position += movement * speed * Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        // Створення екземпляра кулі на точці вистрілу
+        GameObject bullet = Instantiate(BulletLite_02, firePoint.position, firePoint.rotation);
+
+        // Додавання сили вистрілу кулі
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        bulletRb.AddForce(firePoint.forward * 1f, ForceMode.Impulse);
     }
 }
